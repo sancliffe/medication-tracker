7c7bd6cb17297d302d561077565fd1c56e78a147
Stephen Ancliffe <stephen.ancliffe@gmail.com>
2026-05-09 22:48:04 -0400

    Android Gradle Program update and APK build.

--- app/src/main/java/android/template/ui/MainActivity.kt
+++ app/src/main/java/android/template/ui/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -22,7 +21,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
